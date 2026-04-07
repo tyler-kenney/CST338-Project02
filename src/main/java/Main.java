3d5722fd02868e97f94dedb1c60a7e4d7cc8514a
etern-source <etern@csumb.edu>
2026-04-06 19:50:05 -0700

    Started SceneFactory. -Currently 3 different Scenes -Login is working (Feel Free to add your name to txt file to test login) -Changed main to work with JavaFX.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import static javafx.application.Application.launch;
 /**
+ * Main File for Application, includes Application launch.
  * @author :erict
  * <br>
  * Created :4/5/2026
  */
 
-public class Main {
+public class Main extends Application {
     public void start(Stage stage){
-
+        Scene scene = SceneFactory.Create(SceneType.Login, stage);
+        // text shown in the OS title bar
+        stage.setScene(scene);
+        stage.show();       // make the window visible
     }
     public static void main(String[] args) {
-        System.out.println("Hello World");
         launch(args);
     }
 }
---
+++ src/main/java/SceneType.java
@@ -0,0 +1,11 @@
+/**
+ * Used to set names of Scenes. For easy calls/references.
+ * @author :erict
+ * <br>
+ * Created :4/5/2026
+ */
+public enum SceneType {
+    Login,
+    Administrator,
+    General
+}
