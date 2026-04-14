b75172ebc359e9fb5164283a3054e268f7f72067
tyler-kenney <tyler.kenney.tk.2022@gmail.com>
2026-04-14 01:08:41 -0700

    Login logic created. Database Schema implemented. Create new Account implemented.

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -12,11 +12,9 @@ import java.util.List;
 
 public class DatabaseManager {
 
-  //TODO: restructure sqlite file.
-
   // " jdbc : sqlite :" tells JDBC which driver to use .
   // The path after it is the database file location .
-  private static final String DB_URL = " jdbc:sqlite:app.db ";
+  private static final String DB_URL = "jdbc:sqlite:mybd.db";
   private Connection connection;
 
   /**
@@ -29,7 +27,7 @@ public class DatabaseManager {
       System.out.println(" Database connected . ");
       createTables(); // set up schema on first run
     } catch (SQLException e) {
-      System.err.println(" Connection failed : " + e.getMessage());
+      System.out.println(" Connection failed : " + e.getMessage());
     }
   }
 
@@ -51,35 +49,168 @@ public class DatabaseManager {
    */
   private void createTables() {
     // Text blocks ( Java 15+) keep multi - line SQL readable
-    String sql = """
-            CREATE TABLE IF NOT EXISTS items (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            done INTEGER NOT NULL DEFAULT 0,
-                created TEXT DEFAULT (datetime( ’ now ’))
+    //type = 0 or 1 (user or admin)
+    String users_sql = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            usertype INTEGER NOT NULL
+        )
+        """;
+
+    String categories_sql = """
+        CREATE TABLE IF NOT EXISTS categories (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          category TEXT NOT NULL UNIQUE,
+          description TEXT
+        )
+        """;
+
+    String questions_sql = """
+        CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category_id INTEGER NOT NULL REFERENCES categories(id), --foreign key
+            question TEXT NOT NULL UNIQUE,
+            possible_answers TEXT NOT NULL,
+            answer INTEGER NOT NULL,
+            created DATETIME DEFAULT CURRENT_TIMESTAMP,
+            user_id INTEGER NO NULL REFERENCES users(id), --foreign key
+            attempts INTEGER DEFAULT 0
+        )
+        """;
+
+    String question_attempts_sql = """
+        CREATE TABLE IF NOT EXISTS question_attempts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question_id INTEGER NOT NULL REFERENCES questions(id),
+        attempt_id INTEGER NOT NULL REFERENCES quiz_attempts(id),
+        is_correct INTEGER NOT NULL   --0 for no, 1 for yes
+        )
+        """;
+
+    String quiz_attempts_sql = """
+        CREATE TABLE IF NOT EXISTS quiz_attempts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL REFERENCES users(id), --foreign key
+            category_id INTEGER NOT NULL REFERENCES categories(id), --foreign key
+            time_taken DATETIME DEFAULT CURRENT_TIMESTAMP,
+            score DOUBLE NOT NULL
         )
         """;
     try (Statement stmt = connection.createStatement()) {
-      stmt.execute(sql);
+      stmt.execute(users_sql);
+      stmt.execute(categories_sql);
+      stmt.execute(questions_sql);
+      stmt.execute(quiz_attempts_sql);
+      stmt.execute(question_attempts_sql);
+
     } catch (SQLException e) {
-      System.err.println(" createTables failed : " + e.getMessage());
+      System.out.println(" createTables failed : " + e.getMessage());
     }
   }
 
   /**
-   * CRUD - Create (insert)
+   * CRUD - Create (insert). For Table users
    *
-   * @param name
+   * @param username
    */
-  //TODO: refactor to accept more values such as: question, answers, correct_answer, scores.
-  public void insertItem(String name) {
+  public void insertUserItem(String username, String password, int usertype) {
+    if (username == null || password == null) {
+      System.out.println(" Username and password are null . ");
+      return;
+    }
+    String testSql = "INSERT INTO users (username, password, usertype) VALUES (?, ?, ?)";
     String sql = " INSERT INTO items ( name ) VALUES (?) ";
-    try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-      pstmt.setString(1, name); // bind parameter 1 to ’name ’
+    try (PreparedStatement pstmt = connection.prepareStatement(testSql)) {
+      pstmt.setString(1, username);
+      pstmt.setString(2, password);  // bind parameter 1 to ’name ’
+      pstmt.setInt(3, usertype);
       pstmt.executeUpdate();
     } catch (SQLException e) {
-      System.err.println(" insertItem failed : " + e.getMessage());
+      System.out.println(" insertItem failed : " + e.getMessage());
     }
+  }
+
+  /**
+   * This method checks if parameter is in database table users
+   *
+   * @param username
+   * @return
+   */
+  public boolean isUsername(String username) {
+    List<String> users = new ArrayList<>(getAllUsernames());
+    for (String user : users) {
+      if (user.equals(username)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * CRUD - Read (Select)
+   * @param username
+   * @return
+   */
+  public String getPassword(String username) {
+    String password = "";
+
+    String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+    try (Statement statement = connection.createStatement()) {
+      //statement.setString(1, username);
+      ResultSet rs = statement.executeQuery(sql);
+      password = (rs.getString("password"));
+    } catch (SQLException e) {
+      System.out.println(" getPassword failed : " + e.getMessage());
+    }
+    return password;
+  }
+
+  public Boolean isPassword(String username, String password) {
+    return !getPassword(username).isEmpty() && password.equals(getPassword(username));
+  }
+
+  /**
+   * CRUD - Read (Select)
+   * @param username
+   * @param password
+   * @return
+   */
+  public int getUsertype(String username, String password) {
+    int usertype = 0;
+    String sql = "SELECT usertype FROM users WHERE username = '"
+        + username + "' AND password = '" + password + "'";
+    try (Statement statement = connection.createStatement()) {
+      ResultSet rs = statement.executeQuery(sql);
+      usertype = (rs.getInt("usertype"));
+    } catch (SQLException e) {
+      System.out.println("getUsertype failed : " + e.getMessage());
+    }
+    return usertype;
+  }
+
+  public Boolean isAdmin(String username, String password) {
+    return getUsertype(username, password) == 1;
+  }
+
+  /**
+   * CRUD - Read (Select)
+   *
+   * @return
+   */
+  public List<String> getAllUsernames() {
+    List<String> users = new ArrayList<>();
+    String sql = " SELECT username FROM users";
+    try (Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(sql)) {
+      while (rs.next()) { // move to next row
+        users.add(rs.getString("username")); // read column by name
+      }
+    } catch (SQLException e) {
+      System.out.println(" getAllItems failed : " + e.getMessage());
+    }
+    return users;
   }
 
   /**
@@ -96,13 +227,14 @@ public class DatabaseManager {
         items.add(rs.getString(" name ")); // read column by name
       }
     } catch (SQLException e) {
-      System.err.println(" getAllItems failed : " + e.getMessage());
+      System.out.println(" getAllItems failed : " + e.getMessage());
     }
     return items;
   }
 
   /**
    * CRUD - Update.
+   *
    * @param id
    */
   public void markDone(int id) {
@@ -111,13 +243,14 @@ public class DatabaseManager {
       pstmt.setInt(1, id);
       pstmt.executeUpdate();
     } catch (SQLException e) {
-      System.err.println(" markDone failed : " + e.getMessage());
+      System.out.println(" markDone failed : " + e.getMessage());
     }
     //TODO: update number of rows affected.
   }
 
   /**
    * CRUD - Delete.
+   *
    * @param id
    */
   public void deleteItem(int id) {
@@ -126,7 +259,7 @@ public class DatabaseManager {
       pstmt.setInt(1, id);
       pstmt.executeUpdate();
     } catch (SQLException e) {
-      System.err.println(" deleteItem failed : " + e.getMessage());
+      System.out.println(" deleteItem failed : " + e.getMessage());
     }
     //TODO: update number of rows affected.
   }
--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -2,7 +2,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -13,150 +15,261 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Running UI for program, Should handle scene switches.
- * Allow user to log in / out.
- * Allow user to have options to do stuff.
+ * Running UI for program, Should handle scene switches. Allow user to log in / out. Allow user to
+ * have options to do stuff.
  *
  * @author :erict
  * <br>
  * Created :4/5/2026
  */
 public interface SceneFactory {
-    //For UI
-    int SCENE_WIDTH = 800;
-    int SCENE_HEIGHT = 800;
-    int SCENE_PADDING = 30;
-    int INPUT_WIDTH = 200;
 
-    //Local variables.
-    String ERROR = "Not a valid input!";
-    TextField s1Input = new TextField();
-    TextField s2Input = new TextField();
+  //For UI
+  int SCENE_WIDTH = 800;
+  int SCENE_HEIGHT = 800;
+  int SCENE_PADDING = 30;
+  int INPUT_WIDTH = 200;
 
-    //Static Class for setting up users
-    static class User {
-        private String username;
-        private String password;
-        private String role;
+  //Local variables.
+  String ERROR = "Not a valid input!";
 
-        public User(String username, String password, String role) {
-            this.username = username;
-            this.password = password;
-            this.role = role;
+  //Static Class for setting up users
+  static class User {
+
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role) {
+      this.username = username;
+      this.password = password;
+      this.role = role;
+    }
+
+  }
+
+
+  //Class static method Create. Using enum case switch.
+  static Scene Create(SceneType sceneType, Stage stage,
+      DatabaseManager db) {   //TODO: refactor to accept database
+    return switch (sceneType) {
+      case Login -> BuildUserLogin(stage, db);
+      //Most likely won't be reach unless we're testing.
+      case NewUser -> BuildNewAccount(stage, db);
+      case Administrator -> BuildAdminUser(stage, db);
+      case General -> BuildGeneralUser(stage, db);
+
+    };
+  }
+
+  private static Scene BuildNewAccount(Stage stage, DatabaseManager db) {
+    Label PromptNewUserName = new Label("Enter New Username: ");
+    Label PromptNewPassword = new Label("Enter New Password: ");
+
+    TextField s2Input1 = new TextField();
+    TextField s2Input2 = new TextField();
+
+    Label s2Output1 = new Label("");
+
+    s2Input1.setPromptText(PromptNewUserName.getText());
+    s2Input1.setPrefWidth(INPUT_WIDTH);
+    s2Input2.setPromptText(PromptNewPassword.getText());
+    s2Input2.setPrefWidth(INPUT_WIDTH);
+    Button PromptNewAccount = new Button("Create Account");
+    CheckBox adminCheck = new CheckBox("Admin");
+
+    PromptNewAccount.setOnAction(a -> {
+      String username = s2Input1.getText().trim();
+      String password = s2Input2.getText().trim();
+      int role_num;
+      if (adminCheck.isSelected()) {
+        role_num = 1;
+      } else {
+        role_num = 0;
+      }
+      if (!db.isUsername(username)) {
+        db.insertUserItem(username, password, role_num);
+        Scene BackScene = BuildUserLogin(stage, db);
+        stage.setScene(BackScene);
+      } else {
+        Alert AlertCreation = new Alert(AlertType.ERROR);
+        AlertCreation.setTitle("Username Error");
+        AlertCreation.setHeaderText("Username Already Exists");
+        AlertCreation.setContentText("Please choose another username");
+        AlertCreation.showAndWait();
+      }
+    });
+    stage.setTitle("Welcome New User");
+    VBox root2 = new VBox(
+        12,
+        PromptNewUserName,
+        s2Input1,
+        PromptNewPassword,
+        s2Input2,
+        adminCheck,
+        PromptNewAccount,
+        s2Output1);
+    root2.setPadding(new Insets(SCENE_PADDING));
+    root2.setAlignment(Pos.CENTER);
+    return new Scene(root2, SCENE_WIDTH, SCENE_HEIGHT);
+  }
+
+  /**
+   *
+   * @param stage BuildUserLogin
+   * @return User Login screen, prompting user to enter Username and Password.
+   *
+   */
+  private static Scene BuildUserLogin(Stage stage, DatabaseManager db) {
+    Label PromptUserName = new Label("Username: ");
+    Label PromptPassword = new Label("Password: ");
+
+    List<User> users = new ArrayList<>();
+    TextField s1Input1 = new TextField();
+    TextField s1Input2 = new TextField();
+    s1Input1.setPromptText(PromptUserName.getText());
+    s1Input1.setPrefWidth(INPUT_WIDTH);
+    s1Input2.setPromptText(PromptPassword.getText());
+    s1Input2.setPrefWidth(INPUT_WIDTH);
+    Button PromptLogin = new Button("Login");
+    Button PromptNewUser = new Button("Create New Account");
+
+    PromptNewUser.setOnAction(e -> {
+      Scene NewAccount = BuildNewAccount(stage, db);
+      stage.setScene(NewAccount);
+    });
+
+    PromptLogin.setOnAction(a -> {
+      String username = s1Input1.getText().trim();
+      String password = s1Input2.getText().trim();
+
+      if (db.isUsername(username) && db.isPassword(username, password)) {
+        if (db.isAdmin(username, password)) {
+          User currentUser = new User(username, password, "Administrator");
+          Alert Alert = new Alert(AlertType.INFORMATION);
+          Alert.setTitle("Welcome");
+          Alert.setHeaderText("Welcome Administrator");
+          Alert.setContentText("You have successfully logged in");
+          Scene Adminscene = BuildAdminUser(stage, db);
+          stage.setScene(Adminscene);
+        } else {
+          User currentUser = new User(username, password, "User");
+          Alert Alert = new Alert(AlertType.INFORMATION);
+          Alert.setTitle("Welcome");
+          Alert.setHeaderText("Welcome User");
+          Alert.setContentText("You have successfully logged in");
+          Scene Generalscene = BuildGeneralUser(stage, db);
+          stage.setScene(Generalscene);
         }
-    }
+      } else {
+        Alert Alert = new Alert(AlertType.ERROR);
+        Alert.setTitle("Log in Error");
+        Alert.setHeaderText("Username or Password is incorrect.");
+        Alert.setContentText("Please try again.");
+        Alert.showAndWait();
+      }
 
-    //Class static method Create. Using enum case switch.
-    static Scene Create(SceneType sceneType, Stage stage) {   //TODO: refactor to accept database
-        return switch (sceneType) {
-            case Login -> BuildUserLogin(stage);
-            //Most likely won't be reach unless we're testing.
-            case Administrator -> BuildAdminUser(stage);
-            case General-> BuildGeneralUser(stage);
-        };
-    }
+//      try (BufferedReader UsernamePassword = new BufferedReader(
+//          new FileReader("UsernamePasswordTest.txt"))) {
+//        String line;
+//        while ((line = UsernamePassword.readLine()) != null) {
+//          if (line.trim().isEmpty()) {
+//            continue;
+//          }
+//          String[] user = line.split(",");
+//          if (user.length == 3) {
+//            String username = user[0].trim();
+//            String password = user[1].trim();
+//            String role = user[2].trim();
+//
+//            users.add(new User(username, password, role));
+//          } else {
+//            System.out.println(ERROR);
+//          }
+//        }
+//
+//        for (User user : users) {
+//          if (user.username.equals(s1Input1.getText()) && user.password.equals(s1Input2.getText())) {
+//            Alert AlertLogin = new Alert(Alert.AlertType.INFORMATION);
+//            AlertLogin.setTitle("Successfully Logged In!");
+//            if (user.role.equals("Admin")) {
+//              Scene Adminscene = BuildAdminUser(stage, db);
+//              stage.setScene(Adminscene);
+//            }
+//
+//            if (user.role.equals("General")) {
+//              Scene Generalscene = BuildGeneralUser(stage, db);
+//              stage.setScene(Generalscene);
+//            }
+//          }
+//        }
+//      } catch (IOException e) {
+//        System.err.println(e.getMessage());
+//        e.printStackTrace();
+//      }
+    });
+    stage.setTitle("Welcome");
+    VBox root = new VBox(
+        12,
+        PromptUserName,
+        s1Input1,
+        PromptPassword,
+        s1Input2,
+        PromptLogin,
+        PromptNewUser);
+    root.setPadding(new Insets(SCENE_PADDING));
+    root.setAlignment(Pos.CENTER);
+    // Scene holds the layout and defines the window size
+    return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+  }
 
-    /**
-     *
-     * @param stage BuildUserLogin
-     * @return User Login screen, prompting user to enter Username and Password.
-     *
-     */
-    private static Scene BuildUserLogin(Stage stage) {
-        Label PromptUserName = new Label("Username: ");
-        Label PromptPassword = new Label("Password: ");
+  /**
+   *
+   * @param stage BuildAdminUser, builds admin page.
+   * @return interactable Admin Page. Currently Only with Built in back button.
+   */
+  private static Scene BuildAdminUser(Stage stage, DatabaseManager db) {
+    Button Logout = new Button("Logout");
+    stage.setTitle("Administrator Menu");
 
-        List<User> users = new ArrayList<>();
-        s1Input.setPromptText(PromptUserName.getText());s1Input.setPrefWidth(INPUT_WIDTH);
-        s2Input.setPromptText(PromptPassword.getText());s2Input.setPrefWidth(INPUT_WIDTH);
-        Button PromptLogin = new Button("Login");
-        PromptLogin.setOnAction(a -> {
-            try (BufferedReader UsernamePassword = new BufferedReader(new FileReader("UsernamePasswordTest.txt"))){
-                String line;
-                while ((line = UsernamePassword.readLine()) != null) {
-                    if(line.trim().isEmpty()){continue;}
-                    String[] user = line.split(",");
-                    if(user.length == 3){
-                        String username = user[0].trim();
-                        String password = user[1].trim();
-                        String role = user[2].trim();
+    Logout.setOnAction(a -> {
+      Alert AlertLogout = new Alert(Alert.AlertType.INFORMATION);
+      AlertLogout.setTitle("Successfully Logged Out!");
+      AlertLogout.setHeaderText("Logging Out. . . ");
+      AlertLogout.setContentText("You have successfully logged out");
+      AlertLogout.showAndWait();
+      Scene BackScene = BuildUserLogin(stage, db);
+      stage.setScene(BackScene);
+    });
+    //stage.setTitle("Administrator");
+    VBox root = new VBox(12, Logout);
+    root.setPadding(new Insets(SCENE_PADDING));
+    root.setAlignment(Pos.CENTER);
+    // Scene holds the layout and defines the window size
+    return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+  }
 
-                        users.add(new User(username, password, role));
-                    } else {
-                        System.out.println(ERROR);
-                    }
-                }
+  /**
+   *
+   * @param stage BuildGeneralUser, builds admin page.
+   * @return interactable General Page. Currently Only with Built in back button.
+   */
+  private static Scene BuildGeneralUser(Stage stage, DatabaseManager db) {
+    Button Logout = new Button("Logout");
+    stage.setTitle("User Menu");
 
-                for(User user : users){
-                    if(user.username.equals(s1Input.getText()) && user.password.equals(s2Input.getText())){
-                        Alert AlertLogin = new Alert(Alert.AlertType.INFORMATION);
-                        AlertLogin.setTitle("Successfully Logged In!");
-                        if(user.role.equals("Admin")){
-                            Scene Adminscene = BuildAdminUser(stage);
-                            stage.setScene(Adminscene);
-                        }
-
-                        if(user.role.equals("General")){
-                            Scene Generalscene = BuildGeneralUser(stage);
-                            stage.setScene(Generalscene);
-                        }
-                    }
-                }
-            } catch (IOException e){
-                System.err.println(e.getMessage());
-                e.printStackTrace();
-            }
-        });
-        stage.setTitle("Welcome");
-        VBox root = new VBox(12, PromptUserName, s1Input,PromptPassword , s2Input, PromptLogin);
-        root.setPadding(new Insets(SCENE_PADDING));
-        root.setAlignment(Pos.CENTER);
-        // Scene holds the layout and defines the window size
-        return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-    }
-
-    /**
-     *
-     * @param stage BuildAdminUser, builds admin page.
-     * @return interactable Admin Page. Currently Only with Built in back button.
-     */
-    private static Scene BuildAdminUser(Stage stage) {
-        Button Logout = new Button("Logout");
-
-        Logout.setOnAction(a -> {
-            Alert AlertLogout = new Alert(Alert.AlertType.INFORMATION);
-            AlertLogout.setTitle("Successfully Logged Out!");
-            Scene BackScene = BuildUserLogin(stage);
-            stage.setScene(BackScene);
-        });
-        stage.setTitle("Administrator");
-        VBox root = new VBox(12, Logout);
-        root.setPadding(new Insets(SCENE_PADDING));
-        root.setAlignment(Pos.CENTER);
-        // Scene holds the layout and defines the window size
-        return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-    }
-
-    /**
-     *
-     * @param stage BuildGeneralUser, builds admin page.
-     * @return interactable General Page. Currently Only with Built in back button.
-     */
-    private static Scene BuildGeneralUser(Stage stage){
-        Button Logout = new Button("Logout");
-
-        Logout.setOnAction(a -> {
-            Alert AlertLogout = new Alert(Alert.AlertType.INFORMATION);
-            AlertLogout.setTitle("Successfully Logged Out!");
-            Scene BackScene = BuildUserLogin(stage);
-            stage.setScene(BackScene);
-        });
-        stage.setTitle("General");
-        VBox root = new VBox(12, Logout);
-        root.setPadding(new Insets(SCENE_PADDING));
-        root.setAlignment(Pos.CENTER);
-        // Scene holds the layout and defines the window size
-        return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-    }
+    Logout.setOnAction(a -> {
+      Alert AlertLogout = new Alert(Alert.AlertType.INFORMATION);
+      AlertLogout.setTitle("Successfully Logged Out!");
+      Scene BackScene = BuildUserLogin(stage, db);
+      stage.setScene(BackScene);
+    });
+    stage.setTitle("General");
+    VBox root = new VBox(12, Logout);
+    root.setPadding(new Insets(SCENE_PADDING));
+    root.setAlignment(Pos.CENTER);
+    // Scene holds the layout and defines the window size
+    return new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+  }
 
 }
